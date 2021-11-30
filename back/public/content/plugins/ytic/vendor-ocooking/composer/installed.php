3cfafd74620f1ef5197823aee41fc90ac18d64c5
Victor Paumier <victor.paumier@neuf.fr>
2021-11-30 13:49:58 +0100

    gg

---
+++ back/public/content/plugins/ytic/vendor-ocooking/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'dc0c55f9d82f6a055e453881d5644d807219898a',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dc0c55f9d82f6a055e453881d5644d807219898a',
+    ),
+  ),
+);
